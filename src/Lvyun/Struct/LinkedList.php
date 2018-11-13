e5b27c1d4817b1af2497c18edf5aa940910af4ad
lvyun <yunlv.go@gmail.com>
2018-11-13 20:35:03 +0800

    feat(*): Add LinkedList

--- src/Lvyun/Struct/LinkedList.php
+++ src/Lvyun/Struct/LinkedList.php
@@ -4,7 +4,7 @@ namespace Lvyun\Struct;
 
 class LinkedList
 {
-    public $value;
+    public $value = null;
     /**
      * @var \Lvyun\Struct\LinkedList
      */
@@ -17,38 +17,132 @@ class LinkedList
 
     public function get($index)
     {
-        for ($i = 0; $i < $index; $i++) {
+        $current = $this;
+        $i = 0;
+        $value = -1;
 
+        while (true) {
+            if (!$current->value && !$current->next) {
+                break;
+            }
+            if (!$current->next && $i != $index) {
+                break;
+            }
+            if ($index == $i) {
+                $value = $current->value;
+                break;
+            }
+            $current = $current->next;
+            $i++;
         }
+
+        return $value;
     }
 
     public function addAtHead($value)
     {
+        if (!$this->value) {
+            $this->value = $value;
+            return;
+        }
 
+        $current = new LinkedList($this->value);
+        $current->next = $this->next;
+
+        $this->value = $value;
+        $this->next = $current;
     }
 
     public function addAtTail($value)
     {
+        if (!$this->value) {
+            $this->value = $value;
+            return;
+        }
 
+        $current = $this;
+        while ($current->next) {
+            $current = $current->next;
+        }
+        $last = new LinkedList($value);
+        $current->next = $last;
     }
 
     public function addAtIndex($index, $value)
     {
+        $current = $this;
+        $i = 0;
 
+        while (true) {
+            if ($index == $i) {
+                if (!$current->value) {
+                    $this->addAtHead($value);
+                    break;
+                }
+                if (!$current->next) {
+                    $this->addAtTail($value);
+                    break;
+                }
+                $n = new LinkedList($value);
+                $n->next = $current->next;
+                $current->next = $n;
+                break;
+            }
+            $current = $current->next;
+            $i++;
+        }
+
+        return $value;
     }
 
     public function deleteAdHead()
     {
-
+        if (!$this->value) {
+            return;
+        }
+        if (!$this->next) {
+            $this->value = null;
+            $this->next = null;
+        }
+        $this->value = $this->next->value;
+        $this->next = $this->next->next;
     }
 
     public function deleteAtTail()
     {
-
+        if (!$this->value) {
+            return;
+        }
+        $prev = $current = $this;
+        while ($current->next) {
+            if ($current->next) {
+                $prev = $current;
+            }
+            $current = $current->next;
+        }
+        if ($prev === $current) {
+            $this->value = null;
+            $this->next = null;
+        }
+        $prev->next = null;
     }
 
     public function deleteAtIndex($index)
     {
-
+        $current = $prev = $this;
+        $i = 0;
+        while (true) {
+            if ($index == $i) {
+                if ($current->next) {
+                    $prev->next = $current->next;
+                    break;
+                } else {
+                    $this->deleteAtTail();
+                }
+            }
+            $prev = $current;
+            $current = $current->next;
+            $i++;
+        }
     }
 }
--- tests/LinkedListTest.php
+++ tests/LinkedListTest.php
@@ -1,15 +1,65 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: lvyun
- * Date: 18-11-12
- * Time: 下午8:53
- */
 
-namespace Lvyun\Struct;
+use Lvyun\Struct\LinkedList;
 
+include __DIR__ . '/bootstrap.php';
 
-class LinkedListTest extends \PHPUnit_Framework_TestCase
+class LinkedListTest extends \PHPUnit\Framework\TestCase
 {
+    public function testNewLinkedList()
+    {
+        $linkedList = new LinkedList(10);
+        $next = new LinkedList(5);
+        $linkedList->next = $next;
+        $next1 = new LinkedList(34);
+        $next->next = $next1;
 
+        $v = $linkedList->get(5);
+        echo $v, "\n";
+
+        $linkedList->addAtHead(1);
+        $linkedList->addAtTail(100);
+
+        $linkedList->addAtIndex(1, 200);
+        $linkedList->deleteAdHead();
+        $linkedList->deleteAtTail();
+        $linkedList->deleteAtIndex(1);
+
+        var_dump($linkedList);
+    }
+
+    public function testDeleteAdHead()
+    {
+
+    }
+
+    public function testDeleteAtIndex()
+    {
+
+    }
+
+    public function testDeleteAtTail()
+    {
+
+    }
+
+    public function testAddAtHead()
+    {
+
+    }
+
+    public function testAddAtIndex()
+    {
+
+    }
+
+    public function testAddAtTail()
+    {
+
+    }
+
+    public function testGet()
+    {
+
+    }
 }
